061c3280d2186dc4a6a85af2c3cfecf2578d4783
Fredrik Andersson <f.anderzon@gmail.com>
2019-05-07 09:20:36 +0200

    lint

--- hello-world/app.js
+++ hello-world/app.js
@@ -1,23 +1,24 @@
 
-const request = require('superagent')
+const request = require('superagent');
+
 const url = 'http://checkip.amazonaws.com/';
 let response;
 
 // eslint-disable-next-line no-unused-vars
 exports.lambdaHandler = async (event, context) => {
-    try {
-        const ret = await request.get(url);
-        response = {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello deploy',
-                location: ret.text.trim()
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
+  try {
+    const ret = await request.get(url);
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'hello deploy',
+        location: ret.text.trim(),
+      }),
+    };
+  } catch (err) {
+    console.log(err);
+    return err;
+  }
 
-    return response
+  return response;
 };
